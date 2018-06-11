829b410b0160d59ee0912fe145156304fcea2ea2
jisub.yoon <yjisub11@gmail.com>
2018-06-11 23:32:36 +0900

    chat 시도

---
+++ src/goChatExample/goChat.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gaurigshankar/golang-chat/chat"
+	"github.com/gaurigshankar/golang-chat/config"
+)
+
+var configuration config.Configuration
+var serverHostName string
+
+func init() {
+	configuration = config.LoadConfigAndSetUpLogging()
+
+	herokuConfigPort := os.Getenv("PORT")
+	if herokuConfigPort == "" {
+		serverHostName = fmt.Sprintf("%s:%s", configuration.Hostname, strconv.Itoa(configuration.Port))
+	} else {
+		serverHostName = fmt.Sprintf(":%s", herokuConfigPort)
+	}
+	log.Println("The serverHost url", serverHostName)
+
+}
+
+func main() {
+
+	// websocket server
+	server := chat.NewServer()
+	go server.Listen()
+	http.HandleFunc("/messages", handleHomePage)
+	http.HandleFunc("/", handleHomePage)
+
+	http.ListenAndServe(serverHostName, nil)
+
+}
+
+func handleHomePage(responseWriter http.ResponseWriter, request *http.Request) {
+	http.ServeFile(responseWriter, request, "chat.html")
+}
